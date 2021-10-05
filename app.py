472075fa6ce29a3acfcac84a5fbfda48162f176e
Paul Jones Segura <65366055+pjoness@users.noreply.github.com>
2021-10-05 16:55:52 +0000

    agregar archivos

---
+++ app.py
@@ -0,0 +1,87 @@
+from flask import Flask, render_template, flash, request, redirect, url_for
+app = Flask(__name__)
+
+#---------------------------------RUTAS ARBOL PRINCIPAL----------------------------------
+@app.route('/', methods=["GET"])
+def index():
+    return render_template('/RAIZ/index.html')
+
+@app.route('/habitaciones_reservas', methods=["GET"])
+def habitaciones_reservas():
+    return render_template('/RAIZ/habitaciones_reservas.html')
+
+@app.route('/quienes_somos', methods=["GET"])
+def quienes_somos():
+    return render_template('/RAIZ/quienes_somos.html', methods=["GET"])
+
+@app.route('/politicas_de_privacidad', methods=["GET"])
+def politicas_de_privacidad():
+    return render_template('/RAIZ/politicas_de_privacidad.html')
+
+@app.route('/terminos_y_condiciones', methods=["GET"])
+def terminos_y_condiciones():
+    return render_template('/RAIZ/terminos_y_condiciones.html')
+
+
+#---------------------------------RUTAS ESPECIFICAS DE USUARIO----------------------------------
+@app.route('/registro', methods=["GET","POST"])
+def registro():
+    return render_template('/USUARIO/registro.html')
+
+@app.route('/habitacion/', methods=["GET"])
+def habitacion():
+    return render_template('/USUARIO/habitacion.html')
+
+@app.route('/login', methods=["GET","POST"])
+def login():
+    return render_template('/USUARIO/login.html')
+
+@app.route('/perfil', methods=["GET"])
+def perfil():
+    return render_template('/USUARIO/perfil.html')
+
+@app.route('/perfil_reservas', methods=["GET"])
+def perfil_reservas():
+    return render_template('/USUARIO/perfil_reservas.html')
+
+#---------------------------------RUTAS ADMINISTRADOR----------------------------------
+@app.route('/admin_gesuser', methods=["GET"])
+def admin_gesuser():
+    return render_template('/ADMIN/admin_gesuser.html')
+
+@app.route('/admin_geshabitacion', methods=["GET"])
+def admin_geshabitacion():
+    return render_template('/ADMIN/admin_geshabitacion.html')
+
+@app.route('/admin_gescomentario', methods=["GET"])
+def admin_gescomentario():
+    return render_template('/ADMIN/admin_gescomentario.html')
+
+@app.route('/admin_profile', methods=["GET"])
+def admin_profile():
+    return render_template('/ADMIN/admin_profile.html')
+
+#---------------------------------RUTAS SUPER ADMINISTRADOR----------------------------------
+@app.route('/superadmin_gesuser', methods=["GET"])
+def superadmin_gesuser():
+    return render_template('/SUPERADMIN/superadmin_gesuser.html')
+
+@app.route('/superadmin_geshabitacion', methods=["GET"])
+def superadmin_geshabitacion():
+    return render_template('/SUPERADMIN/superadmin_geshabitacion.html')
+
+@app.route('/superadmin_gescomentario', methods=["GET"])
+def superadmin_gescomentario():
+    return render_template('/SUPERADMIN/superadmin_gescomentario.html')
+
+@app.route('/superadmin_gesroles', methods=["GET"])
+def superadmin_gesroles():
+    return render_template('/SUPERADMIN/superadmin_gesroles.html')
+
+@app.route('/superadmin_profile', methods=["GET"])
+def superadmin_profile():
+    return render_template('/SUPERADMIN/superadmin_profile.html')
+
+if __name__=='__main__':
+    # app.run(debug=True)
+    app.run(host='0.0.0.0', port=5001, debug=True)
